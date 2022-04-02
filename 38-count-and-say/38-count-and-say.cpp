161064d2bb11da60da6061e126d7578bfefc094c
jaiganeshs21 <89634744+jaiganeshs21@users.noreply.github.com>
2022-04-02 09:39:56 +0530

    Time: 8 ms (73.65%), Space: 6.6 MB (79.44%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        string s="1";
+        
+        while(--n){
+            string temp="";
+            for(int i=0;i<s.size();++i){
+                int cnt=1;
+                while(i+1<s.size() && s[i]==s[i+1]){
+                    ++cnt;
+                    ++i;
+                }
+                temp+=to_string(cnt)+s[i];
+            }
+            s=temp;
+            
+        }
+        
+        return s;
+    }
+};
