744decd691822026e979033697c6fbf567b0a42f
jaiganeshs21 <89634744+jaiganeshs21@users.noreply.github.com>
2022-08-06 00:52:51 +0530

    Time: 3 ms (62.90%), Space: 6.7 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-int combinationSum4(vector<int>& nums, int target) {
-        /* sorting is used to minimize the run time for iterating through the nums.
-            Once we reach number greater than the target we are looking for, there's no need to search for the rest.
-        */
-        sort(nums.begin(), nums.end());
-        // use unsigned long to prevent integer overflow
-        vector<unsigned long> dp(target + 1, 0);
-        // standing on the ground also count as one way.
-        dp[0] = 1;
-        for (int i = 1; i <= target; ++i){
-            int ways = 0;
-            for (int j = 0; j < nums.size(); ++j){
-                int remain = i - nums[j];
-                if (remain < 0) break;
-                // the way it can reach the point are always start from the smaller number
-                ways += dp[remain];
+    int combinationSum4( vector<int>& nums,int target) {
+        unsigned int dp[target+1]; 
+        fill(dp,dp+target+1,0);
+        int n=nums.size();
+
+        dp[0]=1;  //dp[0] is 1 because there is 1 way to get sum 0 by not selecting anything
+        
+        for(int amt=0;amt<=target ;amt++)
+            for(int j=0;j<n;j++)
+            {
+                if(nums[j]<=amt)
+                    dp[amt]= dp[amt]+dp[amt-nums[j]];
             }
-            dp[i] = ways;
-        }
         return dp[target];
     }
-		
 };
