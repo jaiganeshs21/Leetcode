443cbf6cc366d619a9567e50158ea32d2455b9c2
jaiganeshs21 <89634744+jaiganeshs21@users.noreply.github.com>
2022-08-14 17:34:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (94.03%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int compareVersion(string v1, string v2) {
         int n1=v1.size(),n2=v2.size();
-        int i=0,j=0;
         
+        int i=0,j=0;
         while(i<n1 || j<n2){
             int num1=0,num2=0;
-            
             while(i<n1 && v1[i]!='.'){
                 num1=num1*10+(v1[i]-'0');
-                ++i;     
+                ++i;
             }
             
             while(j<n2 && v2[j]!='.'){
@@ -19,6 +18,7 @@ public:
             
             if(num1>num2) return 1;
             else if(num2>num1) return -1;
+            
             ++i;
             ++j;
         }
